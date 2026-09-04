433632c4ac06fae51b467aeab6c55cf30de877ce
Vishwam Patel <patelvishwam728@gmail.com>
2026-09-04 22:06:42 +0530

    First Setup

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '://cloudinary.com',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
